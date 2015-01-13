3034d16a3d1bc2c13602f6956cbf0b4164365959
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2015-01-14 01:19:00 +0800

    Test for splitting arguments into roots and extensions - Red

--- src/test/ParseTest.java
+++ src/test/ParseTest.java
@@ -2,20 +2,48 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import app.CodeCollate;
 
 public class ParseTest {
-
+	private static final String PATH_VIEW = "/test/sample1/controller/View.cpp.in";
+	private static final String PATH_CONTROLLER_B = "/test/sample1/controller/ControllerB.cpp.in";
+	private static final String PATH_CONTROLLER_A = "/test/sample1/controller/ControllerA.cpp.in";
+	private static final String EXTENSION = "in";
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		//TODO
 	}
 
 	@Test
-	public void test() {
-		//TODO
+	public void testParseInput() {
+		String[] input = new String[] {PATH_CONTROLLER_A, PATH_CONTROLLER_B, PATH_VIEW, EXTENSION};
+		String[] roots = {};
+		String[] extensions = {};
+		CodeCollate collator = new CodeCollate(input);
+		try {
+			Field rootsField = CodeCollate.class.getDeclaredField("_roots");
+			rootsField.setAccessible(true);
+			roots = (String[]) rootsField.get(collator);
+			Field extensionsField = CodeCollate.class.getDeclaredField("_extensions");
+			extensionsField.setAccessible(true);
+			extensions = (String[]) extensionsField.get(collator);
+		} catch (SecurityException | IllegalAccessException | IllegalArgumentException | NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+		assertArrayEquals("Split args array", new String[]{PATH_CONTROLLER_A, PATH_CONTROLLER_B, PATH_VIEW}, roots);
+		assertArrayEquals("Split args array", new String[]{EXTENSION}, extensions);
+		
+		
+	
 	}
 
 }
