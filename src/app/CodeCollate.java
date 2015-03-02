9743a03f02371a6d470e9df0d074697b130e4f9a
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2015-03-03 02:39:56 +0800

    Implement acknowledging code files -green

--- src/app/CodeCollate.java
+++ src/app/CodeCollate.java
@@ -2,7 +2,9 @@ package app;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,6 +12,7 @@ public class CodeCollate {
 	private static Logger log = Logger.getLogger(CodeCollate.class.getSimpleName());
 	private String[] _roots;
 	private String[] _extensions;
+	private List<String> _files;
 	
 	public static void main(String[] args) {
 		CodeCollate collator = new CodeCollate(args);
@@ -17,7 +20,38 @@ public class CodeCollate {
 	}
 	
 	private void collateAll() {
-		// TODO Auto-generated method stub
+		acknowledgeCodeFiles(_roots, _extensions);
+		
+	}
+
+	private void acknowledgeCodeFiles(String[] roots, String[] extensions) {
+		_files = new ArrayList<String>();
+		for (int i = 0; i < roots.length; i++) {
+			addFilesIn(roots[i],extensions);
+		}
+		
+	}
+
+	private void addFilesIn(String rootPath, String[] extensions) {
+		File root = new File(rootPath);
+		if (root.isDirectory()){
+			String[] subPaths = (root.list());
+			for (int i = 0; i < subPaths.length; i++) {
+				if (rootPath.endsWith("/")) {
+					addFilesIn(rootPath + subPaths[i], extensions);
+				} else {
+					addFilesIn(rootPath + "/" + subPaths[i], extensions);
+				}
+			}
+		} else if (root.isFile()) {
+			String fileName = root.getName();
+			String fileExtension = fileName.substring(fileName.lastIndexOf('.')+1);
+			if (Arrays.asList(extensions).contains(fileExtension)) {
+				_files.add(rootPath);
+			}
+		}
+		
+			
 		
 	}
 
