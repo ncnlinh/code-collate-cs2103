40e63881cbaf434b643c36cf2194b57f86141773
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2015-03-10 18:42:06 +0800

    Add more complex test case - red

--- src/test/TestCollate.java
+++ src/test/TestCollate.java
@@ -1,10 +1,15 @@
 package test;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -103,6 +108,18 @@ public class TestCollate {
 
 	@Test
 	public void testCreateCollatedFiles() {
+		input = new String[] {PATH_CONTROLLER_A, PATH_CONTROLLER_B, PATH_VIEW, EXTENSION};	
+		collator = new CodeCollate(input);
+		Object[] files = {};
+		try {
+			Method collateAllMethod = CodeCollate.class.getDeclaredMethod("collateAll");
+			collateAllMethod.setAccessible(true);
+			collateAllMethod.invoke(collator);
+		} catch (NoSuchMethodException | IllegalAccessException | IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
 		List<File> collatedFiles = new ArrayList<File>();
 		collatedFiles.add(new File("collated/" + SAMPLE_AUTHOR_1 +".col"));
 		collatedFiles.add(new File("collated/" + SAMPLE_AUTHOR_2 +".col"));
@@ -112,4 +129,44 @@ public class TestCollate {
 			assertTrue("File #"+(i+1)+" is a file", collatedFile.isFile());
 		}
 	}
+	
+	private static final String TODOLOG_PATH_INPUT = "./test/ToDoLog/input";
+	private static final String TODOLOG_PATH_EXTENSIONS = "java";
+	private static final String TODOLOG_PATH_OUTPUT_EXPECTED = "./test/ToDoLog/output/collated";
+	private static final String[] TODOLOG_AUTHORS = {"a0111513b","a0111608r","a0112156u-reused","a0112156u","a0118899e"};
+	@Test
+	public void testCodeCollated() throws IOException {
+		CodeCollate.main(new String[] {TODOLOG_PATH_INPUT,TODOLOG_PATH_EXTENSIONS});
+		for (int i = 0; i < TODOLOG_AUTHORS.length; i++) {
+			File collatedFile = new File("collated/"+TODOLOG_AUTHORS[i]+".col");
+			assertTrue("File #"+(i+1)+" exists", collatedFile.exists());
+			assertTrue("File #"+(i+1)+" is a file", collatedFile.isFile());
+		}
+//		File expected = new File (TODOLOG_PATH_OUTPUT_EXPECTED);
+//		for (int i = 0; i < TODOLOG_AUTHORS.length; i++) {
+//			File expectedFile = new File("collated/"+TODOLOG_AUTHORS[i]+".col");
+//			File actualFile = new File(TODOLOG_PATH_OUTPUT_EXPECTED+"/"+TODOLOG_AUTHORS[i]+".col");
+//			assertTrue("number of lines of "+TODOLOG_AUTHORS[i],Math.abs(countLines(expectedFile)-countLines(actualFile))<countLines(expectedFile)*0.1);
+//		}
+	}
+//	public static int countLines(File file) throws IOException {
+//	    InputStream is = new BufferedInputStream(new FileInputStream(file));
+//	    try {
+//	        byte[] c = new byte[1024];
+//	        int count = 0;
+//	        int readChars = 0;
+//	        boolean empty = true;
+//	        while ((readChars = is.read(c)) != -1) {
+//	            empty = false;
+//	            for (int i = 0; i < readChars; ++i) {
+//	                if (c[i] == '\n') {
+//	                    ++count;
+//	                }
+//	            }
+//	        }
+//	        return (count == 0 && !empty) ? 1 : count;
+//	    } finally {
+//	        is.close();
+//	    }
+//	}
 }
