cef3fe67c5e93d03c3fa79b2b04da968d203a0c7
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2015-03-10 15:58:53 +0800

    basic implementation of collating, created files - green

--- src/app/CodeCollate.java
+++ src/app/CodeCollate.java
@@ -1,7 +1,14 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,7 +20,13 @@ public class CodeCollate {
 	private String[] _roots;
 	private String[] _extensions;
 	private List<String> _files;
-	
+	private List<String> _collatedFiles;
+	private enum CodeLineType {
+		AUTHOR_COMMENT,
+		COMMENT,
+		REGULAR,
+		BLANK
+	}
 	public static void main(String[] args) {
 		CodeCollate collator = new CodeCollate(args);
 		collator.collateAll();
@@ -21,7 +34,114 @@ public class CodeCollate {
 	
 	private void collateAll() {
 		acknowledgeCodeFiles(_roots, _extensions);
+		collateFiles(_files);
 		
+	}
+	void purgeDirectory(File dir) {
+	    for (File file: dir.listFiles()) {
+	        if (file.isDirectory()) purgeDirectory(file);
+	        file.delete();
+	    }
+	}
+	
+	private void collateFiles(List<String> files) {
+		for (String filePath: files){
+			try {	
+				readCode(filePath);
+			} catch (IOException e) {
+				throw new RuntimeException("Failed to read code file " + filePath);
+			}
+		}
+	}
+
+	private void readCode(String filePath) throws IOException {
+		BufferedReader reader = null;
+		reader = new BufferedReader(new FileReader(filePath));
+		String line = reader.readLine();
+		String currentAuthor = null;
+		while (line != null) {
+			CodeLineType lineType = identifyLine(line);
+			switch (lineType) {
+				case BLANK: // Fallthrough
+				case COMMENT: // Fallthrough
+				case REGULAR:
+					addToAuthor(line,currentAuthor);
+					break;
+				case AUTHOR_COMMENT:
+					if (currentAuthor != null) {
+						insertEndSegment(currentAuthor, filePath);
+					}
+					String[] authorLine = line.split(" ");
+					String author = authorLine[authorLine.length-1];
+					currentAuthor = author;
+					insertStartSegment(currentAuthor, filePath);
+					break;
+			}
+			line = reader.readLine();
+		}
+		insertEndSegment(currentAuthor, filePath);
+		reader.close();
+		
+	}
+
+	private void insertStartSegment(String author, String filePath) throws IOException {
+		File collatedFile = new File("collated/"+author+".col");
+		if (!collatedFile.exists()) {
+			collatedFile.createNewFile();
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(collatedFile,true)));
+			pw.println("//@author "+author);
+			pw.println();
+			pw.println();
+			pw.println();
+			pw.close();
+			
+		}
+		String origin = new File(filePath).getCanonicalPath();
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(collatedFile,true)));
+		pw.println("\t/**");
+		pw.println("\t * origin: " + origin);
+		pw.println("\t */");
+		pw.println();
+		pw.close();
+		
+	}
+
+	private void insertEndSegment(String author, String filePath) throws IOException {
+		File collatedFile = new File("collated/"+author+".col");
+		String origin = new File(filePath).getCanonicalPath();
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(collatedFile,true)));
+		pw.println();
+		pw.println("\t// End of segment: "+origin);
+		pw.println();
+		pw.println();
+		pw.println();
+		pw.println();
+		pw.println();
+		pw.close();
+		
+	}
+
+	private void addToAuthor(String line, String author) throws IOException {
+		if (author!=null) {
+			File collatedFile = new File("collated/"+author+".col");
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(collatedFile,true)));
+			pw.println(line);
+			pw.close();
+		}
+		
+	}
+
+	private CodeLineType identifyLine(String line) {
+		if (line.trim().isEmpty()) {
+			return CodeLineType.BLANK;
+		}
+		if (line.trim().startsWith("//")) {
+			if (line.contains("author")) {
+				return CodeLineType.AUTHOR_COMMENT;
+			}
+			return CodeLineType.COMMENT;
+		} 
+		return CodeLineType.REGULAR;
 	}
 
 	private void acknowledgeCodeFiles(String[] roots, String[] extensions) {
@@ -76,6 +196,7 @@ public class CodeCollate {
 		if (outputFolder.exists()) {
 			if (outputFolder.isDirectory()) {
 				log.log(Level.INFO,"Folder already created");
+				purgeDirectory(outputFolder);
 				return true;
 			} else {
 				System.out.println("There is a file called \"collated\" prevents creation of folder");
